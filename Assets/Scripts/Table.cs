5ed7d40b7a140f2018047a8fb7362313a47934ef
Brvcket <ahmatovbulat@gmail.com>
2022-08-27 16:02:39 +0300

    Scene changes

--- Assets/Scripts/Table.cs
+++ Assets/Scripts/Table.cs
@@ -6,8 +6,6 @@ public class Table : MonoBehaviour
 {
     protected bool CanOpen = false;
 
-    public Rigidbody2D rb;
-
     void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "table")
         {
